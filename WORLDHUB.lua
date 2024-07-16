21e2a7036bb9f996901e57c82c01fbb658c757d0
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-07-16 20:26:45 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -6,7 +6,7 @@ local bloxGameId = 2753915549
 if gameId == tappingGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Tapping%20Legends%20Final.lua"))()
 elseif gameId == bloxGameId then
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Blox%20Fruit.lua"))()
 else
     warn("Sorry this game not supported. try to join supported games. More in discord.")
 end
