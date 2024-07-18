9b46897d17a909d92de3d79022e694caa6e66031
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-07-18 18:10:33 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -1,12 +1,12 @@
 local gameId = game.PlaceId
 local tappingGameId = 15705682243
-local bloxGameId = 2753915549
+local UtdGameId = 5902977746
 
 
 if gameId == tappingGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Tapping%20Legends%20Final.lua"))()
-elseif gameId == bloxGameId then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Blox%20Fruit.lua"))()
+elseif gameId == UtdGameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Ultimate%20Tower%20Defense.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game Not Supported, Join Discord, https://discord.com/invite/psE8EUa9kg")
 end
