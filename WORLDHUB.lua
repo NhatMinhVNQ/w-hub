41e619a65aa7e5b9f067ee7c6d650ec4f02ce826
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-07-23 18:39:59 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -1,10 +1,18 @@
 local gameId = game.PlaceId
 local tappingGameId = 15705682243
 local UtdGameId = 5902977746
-
+local w1GameId = 2753915549
+local w2GameId = 4442272183
+local w3GameId = 7449423635
 
 if gameId == tappingGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Tapping%20Legends%20Final.lua"))()
+if gameId == w1GameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Blox%20Fruit.lua"))()
+if gameId == w2GameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Blox%20Fruit.lua"))()
+if gameId == w3GameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Blox%20Fruit.lua"))()
 elseif gameId == UtdGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Ultimate%20Tower%20Defense.lua"))()
 else
