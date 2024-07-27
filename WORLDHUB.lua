6bec90a07afb313cb4b17e22d574e180974b2f3e
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-07-27 08:46:02 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -1,7 +1,6 @@
 local gameId = game.PlaceId
 local tappingGameId = 15705682243
 local UtdGameId = 5902977746
-local ADSGameId = 15968393246
 local w1GameId = 2753915549
 local w2GameId = 4442272183
 local w3GameId = 7449423635
@@ -10,8 +9,6 @@ if gameId == tappingGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Tapping%20Legends%20Final.lua"))()
 elseif gameId == UtdGameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Ultimate%20Tower%20Defense.lua"))()
-elseif gameId == ADSGameId then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Anime%20Destiny%20Simulator.lua"))()
 elseif gameId == w1GameId then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TrumMocBoc/VnWorldHub/main/Vn%20World%20Hub.lua"))()
 elseif gameId == w2GameId then
