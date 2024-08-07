111c2a7a223c1bc586397cc332087a64ee1f2c93
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-08-07 21:14:16 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -1,2 +1,9 @@
-    game.Players.LocalPlayer:Kick("Scripts Down Update, Sorry everyone...,")
+local tappingGameId = 15705682240
+local UtdGameId = 5902977740
+
+if gameId == tappingGameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Tapping%20Legends%20Final.lua"))()
+elseif gameId == UtdGameId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NhatMinhVNQ/w-hub/main/Ultimate%20Tower%20Defense.lua"))()
+        game.Players.LocalPlayer:Kick("Scripts Down Update, Sorry everyone...,")
 end
