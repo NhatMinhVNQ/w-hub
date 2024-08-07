16ccefbe0be0b6f65e89a6a58045474dbb953f98
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-08-08 00:05:05 +0800

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 5902977746 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/ElgatoHub/FS.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
 elseif game.PlaceId == 10260193230 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/ElgatoHub/MM.lua"))();
 else
