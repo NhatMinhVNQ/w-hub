cdbec549b506c3af996ffed4ff4be352f80d5533
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-08-07 21:55:42 +0800

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,7 +1,7 @@
 local player = game.Players.LocalPlayer
 
 if(game.PlaceId == 15705682243) then
-      print("")    
+      print("loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Tapping%20Legends%20Final.lua"))()")    
 
 else
       player:Kick("not Support game")
