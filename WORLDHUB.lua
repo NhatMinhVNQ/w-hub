e3a209fa2b590ce865e20928ef364baca46b6f85
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-08-09 15:58:05 +0800

    Update WORLDHUB.lua

--- WORLDHUB.lua
+++ WORLDHUB.lua
@@ -1,3 +1,9 @@
-local player = game.Players.LocalPlayer
-
-player:Kick("script down update sorry everyone")
+if game.PlaceId == 15705682243 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Tapping%20Legends%20Final.lua"))();
+elseif game.PlaceId == 5902977746 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
+elseif game.PlaceId == 16480898254 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua"))();
+else
+  warn("Game is not supported!");
+end;
