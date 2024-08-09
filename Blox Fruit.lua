29098cedeff5c9b7593a5823ba398ab7110674e4
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-08-09 14:56:31 +0800

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -3,5 +3,5 @@ if game.PlaceId == 15705682243 then
 elseif game.PlaceId == 5902977746 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
 else
-  warn("Game is not supported!");
+  player:Kick("script down update sorry everyone")
 end;
