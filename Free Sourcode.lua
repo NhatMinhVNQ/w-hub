de953b23061eb8c488c00373ce69bc2d2a98dbf8
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2024-11-26 00:59:47 +0800

    Create Free Sourcode.lua

---
+++ Free Sourcode.lua
@@ -0,0 +1,27 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local function checkForPlayer()
+    for _, player in pairs(Players:GetPlayers()) do
+        if player.Name == "VoMinh132" then
+
+            local chatRemote = game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents")
+            if chatRemote and chatRemote:FindFirstChild("SayMessageRequest") then
+                chatRemote.SayMessageRequest:FireServer("Hi VoMinh132 ! :3", "All")
+            end
+            break
+        end
+    end
+end
+
+-- Kiểm tra khi vào game
+checkForPlayer()
+
+Players.PlayerAdded:Connect(function(player)
+    if player.Name == "VoMinh132" then
+        local chatRemote = game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents")
+        if chatRemote and chatRemote:FindFirstChild("SayMessageRequest") then
+            chatRemote.SayMessageRequest:FireServer("Hi VoMinh132 ! :3", "All")
+        end
+    end
+end)
