95368919178c620b30921e6a75f6ae248b92e38f
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2025-01-27 13:41:15 +0800

    Create TP - ISLAND.lua

---
+++ Go Fishing Data/TP - ISLAND.lua
@@ -0,0 +1,14 @@
+local locations = {
+    ["Default Isle"] = Vector3.new(787.944763, 124.980827, -233.17717),
+    ["Vulaco Isle"] = Vector3.new(230.582993, 125.957428, 919.024658),
+    ["Snowy Blome"] = Vector3.new(2326.71289, 127.111076, 338.652771),
+    ["Deep Waters"] = Vector3.new(-993.995605, 123.951134, -1570.35461),
+    ["Ancient Ocean"] = Vector3.new(606.9021, 125.31601, -2918.27051),
+    ["High Field"] = Vector3.new(2575.20874, 123.854439, -3447.85547),
+    ["Toxic Zone"] = Vector3.new(4678.48584, 125.657158, -2453.9082),
+    ["Mansion Island"] = Vector3.new(5214.72656, 129.558258, 649.487915),
+    ["Shipwreck Island"] = Vector3.new(2873.74902, 127.128845, 3046.96436),
+    ["Rough Water Island"] = Vector3.new(16.908577, 127.651413, 2765.45288),
+    ["Stormbreak Island"] = Vector3.new(-2130.62207, 128.939178, 1745.71765,),
+    ["Ghost Ship"] = Vector3.new(-1273.56018, 127.358421, 1233.40137),
+}
