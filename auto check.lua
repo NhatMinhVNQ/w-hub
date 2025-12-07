00212756efc8d9b1692de5ce44d30ec51c669696
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2025-12-08 07:16:38 +0800

    Remove status indicator from 'Eat The World'

--- auto check.lua
+++ auto check.lua
@@ -1,1 +1,1 @@
-[ SUPPORT GAME: ]\nEat The World [🟢]\nGo Fishing [🔵]\nLegends Of Speed [🟢]\nUltimate Tower Defense [🟢]\nSorcerer Tower Defense [🟢]\nAnime Story [🟢]
+[ SUPPORT GAME: ]\nEat The World []\nGo Fishing [🔵]\nLegends Of Speed [🟢]\nUltimate Tower Defense [🟢]\nSorcerer Tower Defense [🟢]\nAnime Story [🟢]
